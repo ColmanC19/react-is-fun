9636104187746caa7ed34058357912a40ee5b443
Colman Currie <curriecolman@gmail.com>
2020-06-01 16:34:06 -0700

    learning course, going over the basics of react as part of a pathway to learn react VR

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+let bookList = [
+  {"title": "The Sun also Rises", "author": "Ernest Hemingway", "pages": 260},
+  {"title": "White Teeth", "author": "Zadie Smith", "pages": 480},
+  {"title": "Hunger", "author": "Roxanne Gay", "pages": 320},
+  {"title": "Cat's Cradle", "author": "Kurt Vonnegut", "pages": 304}
+]
+
+const Book = ({title, author, pages}) => {
+  return (
+    <section>
+      <h2>{title}</h2>
+       <p>{author}</p>
+       <p>{pages}</p>
+    </section>
+  )
+}
+
+class Library extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: true 
+    }
+  }
+  render() {
+    const { books } = this.props
+    return (
+      <div>
+        <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
+      {books.map(
+        (book, i) => <Book
+           title={book.title} 
+           author={book.author} 
+           pages={book.pages} />
+  
+      )}
+    </div>
+
+    )
+
+
+  }
+}
+
+render(
+  <Library books={bookList}/>,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  )
